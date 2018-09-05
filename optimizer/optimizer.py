06931150ac306f527725f6c51861fb26137ff662
elda27 <kaz.birdstick@gmail.com>
2018-09-05 20:17:43 +0900

    Modify some functions

--- optimizer/optimizer.py
+++ optimizer/optimizer.py
@@ -1,13 +1,43 @@
 from abc import abstractmethod, ABCMeta
+from typing import Callable
+
+import numpy as np
 
 
 class Optimizer(metaclass=ABCMeta):
     def __init__(self):
+        self.minimal = None
         self.frameworks = []
         self.optimizing = True
 
     def add_framework(self, framework):
         self.frameworks.append(framework)
+
+    def set_initial_guess(self, x0: np.ndarray):
+        self.initial_guess = x0
+
+    def set_deserialization(self, deserializer: Callable[list]):
+        """Set deserializer in order to restore the structure
+        of optimizing variable.
+
+        Args:
+            deserializer (Callable[list]): A callable object 
+                in order to deserialize list to dictionary format.
+        """
+
+        self.deserializer = deserializer
+
+    def get_minimal(self)->np.ndarray:
+        return self.minimal
+
+    def get_deserialized_minimal(self)->dict:
+        if hasattr(self, 'deserializer'):
+            return self.deserializer(self.minimal)
+        return {'param': self.minimal}
+
+    def setup(self):
+        # Do nothing on base class
+        pass
 
     def generate(self):
         while self.optimizing:
